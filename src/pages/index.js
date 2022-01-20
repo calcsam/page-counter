0766071c0934027babbe35fcab9f6af071b279f4
Sam Bhagwat <calcsam@gmail.com>
2022-01-20 14:39:48 -0800

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import * as React from "react"
+import { useState } from "react"
+import fetch from "node-fetch"
+
 
 // styles
 const pageStyles = {
@@ -11,6 +14,14 @@ const headingStyles = {
   marginBottom: 64,
   maxWidth: 320,
 }
+
+const inputStyles = {
+  padding: 4,
+  fontSize: "1.25rem",
+  borderRadius: 4,
+  backgroundColor: "#FFF4DB",
+}
+
 const headingAccentStyles = {
   color: "#663399",
 }
@@ -126,57 +137,41 @@ const links = [
 ]
 
 // markup
+
+const getNumUrls = async (rawUrl) => {
+  console.log(rawUrl)
+  const url = rawUrl.indexOf(rawUrl.length - 1) === "/" ? rawUrl.concat('sitemap.xml') : rawUrl.concat('/sitemap.xml')
+  const data = await fetch(url).then(response => response.text())
+  console.log(data)
+
+  return data.match(/http/g).length
+}
+
 const IndexPage = () => {
+  const [inputSite, setInputSite] = useState(null);
+  const [site, setSite] = useState(null);
+  const [numPages, setNumPages] = useState(null);
+
+  const processClick = async () => { 
+    const numUrls = await getNumUrls(inputSite); 
+    setNumPages(numUrls); 
+    setSite(inputSite) 
+  }
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
+        How many pages is this site?
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <input onChange={(event) => {setInputSite(event.target.value)}} style={inputStyles}>
+        </input>
+      
+      <button onClick={processClick}>
+        Submit
+      </button>
+      {
+        site ? `Your site ${site} has ${numPages} pages` : null
+      }
     </main>
   )
 }
