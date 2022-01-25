8b0b5a0890da31c01fddd1a9bb0af1c9e12f299b
Sam Bhagwat <calcsam@gmail.com>
2022-01-25 13:19:39 -0800

    add sitemap_index

--- src/api/get-pages.js
+++ src/api/get-pages.js
@@ -8,11 +8,24 @@ const getPage = async (req, res) => {
         'https://'.concat(url)
     console.log(prependedUrl)
     const resp = await fetch(prependedUrl)
+    console.log(resp.status)
+    console.log(resp.url)
+    console.log(resp.redirected)
     if(resp.status === 404) {
         res.status(200).json({ numUrls: null })
+    } else if (resp.redirected && resp.url.match(/sitemap_index/g).length) {
+        const data = await resp.text()
+        const urls = data.match(/<loc>http.*</g).map(url => url.slice(5, url.length -1))
+        let numUrls = 0;
+        let nestedUrlData;
+        for (let index = 0; index < urls.length; index++) {
+            nestedUrlData = await fetch(urls[index]).then(resp => resp.text())
+            numUrls += nestedUrlData.match(/<loc>/g).length
+        }
+        res.status(200).json({ numUrls })
     } else {
         const data = await resp.text()
-        res.status(200).json({ numUrls: data.match(/http/g).length })
+        res.status(200).json({ numUrls: data.match(/<loc>/g).length })
     }    
 }
 
